10176450028b1c3ad50ab8794eca91e5201bb607
Yongjie Xie 谢师傅 <xieyongjie2012@163.com>
2025-01-10 02:50:32 -0500

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import svelte from '@astrojs/svelte';
 
 import tailwind from '@astrojs/tailwind';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), partytown()]
 });
